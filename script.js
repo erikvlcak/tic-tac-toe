955032f424c1dcde5ec4ed2d29f3c22853ee867d
erikvlcak <erikvlcak1@gmail.com>
2023-10-07 18:54:28 +0200

    Add IIFE to remember number of games

--- script.js
+++ script.js
@@ -2,16 +2,41 @@
 
 document.querySelector('nav .startGame').addEventListener('click', () => {
     showBoard();
-
+    console.log(NumberOfGames.getNumberOfGames());
+    createBoard();
 })
 
 document.querySelector('main .options .menuBtn').addEventListener('click', () => {
     showMenu()
-
 })
 
-function createPlayer() {
+// document.querySelector('nav .gameMode').addEventListener('click', () => {
+//     let sp = document.querySelector('.sp');
+//     let mp = document.querySelector('.mp');
+//     if (!(sp.classList.contains('checked'))) {
+//         sp.classList.add('checked')
+//         mp.classList.remove('checked');
+//     }
+//     if (!(mp.classList.contains('checked'))) {
+//         mp.classList.add('checked')
+//         sp.classList.remove('checked');
+//     }
+// })
 
+
+
+function createBoard() {
+    let board = document.querySelector('main .board');
+    board.style.gridTemplateColumns = 'repeat(3, 10vw)';
+    board.style.gridTemplateRows = 'repeat(3, 10vw)';
+    board.style.width = '10wv';
+    for (let i = 1; i <= 9; i++) {
+        let cell = document.createElement('div');
+        cell.style.width = '1fr';
+        cell.style.height = '1fr';
+        cell.classList.add(i);
+        board.appendChild(cell);
+    }
 }
 
 function showBoard() {
@@ -28,4 +53,68 @@ function showMenu() {
     document.querySelector('.rightArea').style.display = 'none';
 }
 
-function 
+
+
+document.querySelector('.gameMode').addEventListener("click", (e) => {
+
+    if ((e.target.className == "sp") || (e.target.className == 'spImg')) {
+        highlightSp()
+        document.querySelector('#nameO').value = 'Computer'
+        document.querySelector('#nameO').disabled = true;
+
+    }
+    if ((e.target.className == "mp") || (e.target.className == 'mpImg')) {
+        highlightMp()
+        document.querySelector('#nameO').value = '';
+        document.querySelector('#nameO').disabled = false;
+    }
+})
+
+document.querySelector('.playerSymbol').addEventListener('click', (e) => {
+
+    if (e.target.className == 'spXO') {
+        e.target.textContent = 'O';
+        document.querySelector
+    }
+
+})
+
+let NumberOfGames = (() => {
+    let selectedNumber = null;
+
+    document.querySelector('.numberOfGames div').addEventListener('click', (e) => {
+        let clearNumbers = (e) => {
+            if (e.target.tagName == 'BUTTON') {
+                for (let i = 0; i < e.target.parentElement.children.length; i++) {
+                    e.target.parentElement.children[i].classList.replace('selected', 'notSelected');
+                }
+            }
+        }
+
+        let number = (e) => {
+            e.target.classList.replace('notSelected', 'selected');
+            selectedNumber = e.target.textContent;
+        }
+
+        clearNumbers(e);
+        number(e);
+    });
+
+    let getNumberOfGames = () => +selectedNumber;
+
+    return { getNumberOfGames };
+})();
+
+function highlightSp() {
+    document.querySelector('.sp').style.backgroundColor = 'black';
+    document.querySelector('.spImg').style.filter = 'invert(100)';
+    document.querySelector('.mp').style.backgroundColor = 'white';
+    document.querySelector('.mpImg').style.filter = 'invert(0)';
+}
+
+function highlightMp() {
+    document.querySelector('.mp').style.backgroundColor = 'black';
+    document.querySelector('.mpImg').style.filter = 'invert(100)';
+    document.querySelector('.sp').style.backgroundColor = 'white';
+    document.querySelector('.spImg').style.filter = 'invert(0)';
+}
